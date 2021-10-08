b7431d9e8fb5f42f54b36f1b9e704ceb29f83667
newasad <asadhmeed2@gmail.com>
2021-10-08 16:54:07 +0300

    add event listener to  buttons

--- js/index.js
+++ js/index.js
@@ -1,71 +1,98 @@
 
-async function getData(){
-    let data1={};
-    let data2={};
-    let dataObjects={};
-    if (!(localStorage.getItem('asia') && localStorage.getItem('europe') && localStorage.getItem('africa') && localStorage.getItem('oceania') && localStorage.getItem('americas') )){
+
+let myChart =null;
+let region="";
+async function getData() {
+    let data1 = {};
+    let data2 = {};
+    let dataObjects = {};
+    if (!(localStorage.getItem('asia') && localStorage.getItem('europe') && localStorage.getItem('africa') && localStorage.getItem('oceania') && localStorage.getItem('americas'))) {
         data1 = await (await fetch('https://corona-api.com/countries')).json();
         data2 = await (await fetch('https://cors-anywhere.herokuapp.com/https://restcountries.herokuapp.com/api/v1')).json();
-    let index =0;
-        dataObjects =Array.from(data1.data).map((item,i)=>{
-        ++index;
-        return{
-            name: data2[i].name.common,
-            region: data2[i].region,
-            code:item.code,
-            confirmed: item.latest_data.confirmed,
-            critical: item.latest_data.critical,
-            deaths: item.latest_data.deaths,
-            recovered: item.latest_data.recovered,
-            population: item.population,
-            today: {
-                deaths: item.today.deaths,
-                confirmed: item.today.confirmed
+        let index = 0;
+        dataObjects = Array.from(data1.data).map((item, i) => {
+            ++index;
+            return {
+                name: data2[i].name.common,
+                region: data2[i].region,
+                code: item.code,
+                confirmed: item.latest_data.confirmed,
+                critical: item.latest_data.critical,
+                deaths: item.latest_data.deaths,
+                recovered: item.latest_data.recovered,
+                population: item.population,
+                today: {
+                    deaths: item.today.deaths,
+                    confirmed: item.today.confirmed
+                }
             }
-        }
-    })
-    
-    dataObjects = dataObjects.filter(el=>el.region.length>0);
-    let asia = dataObjects.filter(el=>el.region==="Asia");
-    let europe = dataObjects.filter(el=>el.region==="Europe");
-    let africa = dataObjects.filter(el=>el.region==="Africa");
-    let oceania = dataObjects.filter(el=>el.region==="Oceania");
-    let americas = dataObjects.filter(el=>el.region==="Americas");
-    localStorage.setItem("asia", JSON.stringify(asia))
-    localStorage.setItem("europe", JSON.stringify(europe))
-    localStorage.setItem("africa", JSON.stringify(africa))
-    localStorage.setItem("oceania", JSON.stringify(oceania))
-    localStorage.setItem("americas", JSON.stringify(americas))
+        })
+
+        dataObjects = dataObjects.filter(el => el.region.length > 0);
+        let asia = dataObjects.filter(el => el.region === "Asia");
+        let europe = dataObjects.filter(el => el.region === "Europe");
+        let africa = dataObjects.filter(el => el.region === "Africa");
+        let oceania = dataObjects.filter(el => el.region === "Oceania");
+        let americas = dataObjects.filter(el => el.region === "Americas");
+        localStorage.setItem("asia", JSON.stringify(asia))
+        localStorage.setItem("europe", JSON.stringify(europe))
+        localStorage.setItem("africa", JSON.stringify(africa))
+        localStorage.setItem("oceania", JSON.stringify(oceania))
+        localStorage.setItem("americas", JSON.stringify(americas))
     }
 
 }
- getData()
-
-function addEventListnerToButtons(){
-
+getData()
+addEventListnerToButtons();
+function addEventListnerToButtons() {
+    document.querySelector('#asia-btn').addEventListener('click',() => updateChart("asia"), false);
+    document.querySelector('#europe-btn').addEventListener('click',() => updateChart("europe"), false);
+    document.querySelector('#americas-btn').addEventListener('click',() => updateChart("americas"), false);
+    document.querySelector('#africa-btn').addEventListener('click',() => updateChart("africa"), false);
+    document.querySelector('#oceania-btn').addEventListener('click',() => updateChart("oceania"), false);
+    updateChart("asia");
+    document.querySelector('#confirmed-btn').addEventListener('click', () => updateChart(region,'confirmed'))
+    document.querySelector('#recovered-btn').addEventListener('click', () => updateChart(region, 'recovered'))
+    document.querySelector('#critical-btn').addEventListener('click', () => updateChart(region, 'critical'))
+    document.querySelector('#deaths-btn').addEventListener('click', () => updateChart(region, 'deaths'))
 }
-setCart();
-function setCart(covidData){
+
+function updateChart(typeOfAddedData, typeOfCases = "confirmed") {
+    let names = [];
+    let numbers = [];
+    region=typeOfAddedData;
+     Array.from(JSON.parse(localStorage.getItem(typeOfAddedData))).map(el => {
+        names = [...names, el.name];
+        numbers = [...numbers, el[typeOfCases]]
+    })
+    setChart({ names: names, numbers: numbers }) ;
+}
+
+
+function setChart(covidData) {
     let ctx = document.querySelector('#myChart').getContext('2d');
-    let myChart = new Chart(ctx, {
+    if(myChart){
+        myChart.destroy();
+    }
+     myChart= new Chart(ctx, {
         type: 'line',
         data: {
-            labels: ['asad', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: covidData.names,
             datasets: [{
                 label: 'Covid 19 World Data',
-            data: [12, 19,3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-            ],
-            borderWidth: 1,
-            tension: 0.4,
-            fill:true,
-                responsive:true
-        }]
-    },
+                data: covidData.numbers,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                ],
+                borderWidth: 1,
+                tension: 0.4,
+                fill: true,
+                responsive: true
+            }]
+        },
         options: {
             plugins: {
                 tooltip: {
@@ -74,8 +101,9 @@ function setCart(covidData){
                     display: true,
                     text: 'Covid 19 World Data',
                 }
-                
+
             }
         }
-});
+    });
+    return myChart;
 }
