02c30ef7288028eaa6c1cdb24d1d4f84c57782a8
newasad <asadhmeed2@gmail.com>
2021-10-09 01:29:09 +0300

    start to build the footer

--- js/index.js
+++ js/index.js
@@ -6,12 +6,13 @@ async function getData() {
     let data1 = {};
     let data2 = {};
     let dataObjects = {};
-    if (!(localStorage.getItem('asia') && localStorage.getItem('europe') && localStorage.getItem('africa') && localStorage.getItem('oceania') && localStorage.getItem('americas'))) {
-        data1 = await (await fetch('https://corona-api.com/countries')).json();
-        data2 = await (await fetch('https://cors-anywhere.herokuapp.com/https://restcountries.herokuapp.com/api/v1')).json();
-        let index = 0;
-        dataObjects = Array.from(data1.data).map((item, i) => {
-            ++index;
+    if (!(localStorage.getItem('asia') && localStorage.getItem('europe') && localStorage.getItem('africa') && localStorage.getItem('oceania') && localStorage.getItem('americas')&& localStorage.getItem('world'))) {
+        try{
+            data1 = await (await fetch('https://corona-api.com/countries')).json();
+            data2 = await (await fetch('https://api.allorigins.win/raw?url=https://restcountries.herokuapp.com/api/v1')).json();
+            console.log(data2);
+ dataObjects = Array.from(data1.data).map((item, i) => {
+          
             return {
                 name: data2[i].name.common,
                 region: data2[i].region,
@@ -34,12 +35,19 @@ async function getData() {
         let africa = dataObjects.filter(el => el.region === "Africa");
         let oceania = dataObjects.filter(el => el.region === "Oceania");
         let americas = dataObjects.filter(el => el.region === "Americas");
+        localStorage.setItem("world", JSON.stringify(dataObjects))
         localStorage.setItem("asia", JSON.stringify(asia))
         localStorage.setItem("europe", JSON.stringify(europe))
         localStorage.setItem("africa", JSON.stringify(africa))
         localStorage.setItem("oceania", JSON.stringify(oceania))
         localStorage.setItem("americas", JSON.stringify(americas))
+   
+        }catch(error){
+            console.log(error);
+
+        }
     }
+       
 
 }
 getData()
@@ -50,7 +58,8 @@ function addEventListnerToButtons() {
     document.querySelector('#americas-btn').addEventListener('click',() => updateChart("americas"), false);
     document.querySelector('#africa-btn').addEventListener('click',() => updateChart("africa"), false);
     document.querySelector('#oceania-btn').addEventListener('click',() => updateChart("oceania"), false);
-    updateChart("asia");
+    document.querySelector('#world-btn').addEventListener('click', () => updateChart("world"), false);
+    updateChart("world");
     document.querySelector('#confirmed-btn').addEventListener('click', () => updateChart(region,'confirmed'))
     document.querySelector('#recovered-btn').addEventListener('click', () => updateChart(region, 'recovered'))
     document.querySelector('#critical-btn').addEventListener('click', () => updateChart(region, 'critical'))
@@ -89,21 +98,34 @@ function setChart(covidData) {
                 ],
                 borderWidth: 1,
                 tension: 0.4,
-                fill: true,
-                responsive: true
+               
             }]
         },
         options: {
+            fill: true,
+            responsive: true,
+            maintainAspectRatio: false,
             plugins: {
                 tooltip: {
-                    enabled: false
+                    enabled: true
                 }, title: {
                     display: true,
                     text: 'Covid 19 World Data',
+                    font:{
+                        size:10
+                    }
                 }
-
             }
         }
     });
     return myChart;
+}
+
+
+function createFooter() {
+let footer = document.querySelector('#footer');
+    let countries = JSON.parse(localStorage.getItem(region));
+let countriesString = Array.from(countries).map(el=>{
+    
+})
 }
